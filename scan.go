e8b0cf73a622584c84f8c3a7d51b530152595f66
Adam Bidwell <adam.bidwell@kurtgeiger.com>
2018-11-09 16:54:33 +0000

    Initial version

---
+++ scan.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/route53"
+    "github.com/aws/aws-sdk-go/aws/session"
+)
+
+func exitErrorf(msg string, args ...interface{}) {
+    fmt.Fprintf(os.Stderr, msg+"\n", args...)
+    os.Exit(1)
+}
+
+func scan (ip string) {
+	fmt.Printf("Scanning for %s\n", ip)
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+
+	svc := route53.New(sess)
+
+	l, err := svc.ListHostedZones(&route53.ListHostedZonesInput{})
+	if err != nil {
+		exitErrorf("Unable to get hosted zones list, %v.", err)
+	}
+	for _, v := range l.HostedZones {
+		//Populate
+		fmt.Printf("Scanning %s...\n", *v.Name)
+		r, err := svc.ListResourceRecordSets(&route53.ListResourceRecordSetsInput{ HostedZoneId: v.Id })
+		if err != nil {
+			exitErrorf("Unable to get hosted zones list, %v.", err)
+		}
+		for _, record := range r.ResourceRecordSets {
+			fmt.Printf("%v\n", record)
+		}
+	}
+}
+
+func main() {
+	scan(os.Args[1])
+}
